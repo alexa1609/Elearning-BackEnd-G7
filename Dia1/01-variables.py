018d06c4fda5548a1c79441ffea50891bedca33b
alexa1609 <32855647+alexa1609@users.noreply.github.com>
2022-06-17 11:14:56 -0500

    se agrego archivo variables

---
+++ Dia1/01-variables.py
@@ -0,0 +1,39 @@
+#Esto es un comentario en Python
+
+nombre = 'Eduardo'
+edad = 29
+
+apellido = "de Rivero"
+
+saludo = "aloh'a a todos"
+saludo = 'aloh"a a todos'
+
+despedida = """bueno amigos,
+se me cuidan mucho,
+atentamente,
+su prosor"""
+
+despedida = '''bueno amigos,
+se me cuidan mucho,
+atentamente,
+su prosor'''
+
+#None > vacio
+
+fecha_de_defuncion = None #indicamos que la variable va a ser nula
+
+#Python es muy sensible a las tabulaciones
+#Si queremos crear varias variables en una misma linea tiene que ser el mismo numero de variables que le numero de contenidos
+
+mascota1, mascota2, gatito1 = 'morocha', 'lulu', 'venecia'
+
+mes, temporada = 6, 'invierno'
+
+soltero = True
+
+soltero = False
+
+#para ver el tipo de dato de una variable
+print(type(mes))
+
+#en todos los lenguajes se recomienda llevar el mismo formato de fecha 
